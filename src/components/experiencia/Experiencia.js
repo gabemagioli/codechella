aae57615ac3b2df0b75afc83c197348e4640835a
Gabriel Magioli <gabrielmagioli1@gmail.com>
2023-05-30 18:37:03 -0300

    commit

--- src/components/experiencia/Experiencia.js
+++ src/components/experiencia/Experiencia.js
@@ -2,6 +2,7 @@ import bannerExperiencia from "./bannerExperiencia.png";
 import sustentabilidade from "./sustentabilidade.png"
 import "./experiencia.css";
 import pessoa from "./pessoa.png";
+import rodaGigante from "./rodaGigante.png"
 
 
 const Experiencia = () => {
@@ -29,6 +30,13 @@ const Experiencia = () => {
                         <p className="p-experiencia2">Queremos estar aqui celebrando daqui a 50, 100 anos! Por isso, levamos o futuro do nosso planeta a sério. Nosso festival é carbono zero, com utilização de copos retornáveis, coleta e destinação adequada de resíduos, e transporte coletivo.</p>
                     </div>
                     <img src={sustentabilidade} alt="pessoa feliz" className="foto-experiencia2"/>
+                </div>
+                <div className="itens">
+                    <img src={rodaGigante} alt="pessoa feliz" className="foto-experiencia"/>
+                    <div className="texto-ex">
+                        <h6 className="info-experiencia">Atracoes</h6>
+                        <p className="p-experiencia">Intervenções artísticas e culturais para todos os gostos! Durante os intervalos dos shows, nossa experiência contará com recreação para crianças, oficina de cerâmica, origami, circo e aquarela! Além disso, nosso espaço contra com 3 praças de alimentação, roda gigante e outros brinquedos!</p>
+                    </div>
                 </div>
             </section>
         </main>
