d727029c2962e8d72b0c86b5cf6e65b516e5ae8a
Gabriel Magioli <gabrielmagioli1@gmail.com>
2023-05-30 18:17:35 -0300

    commit

--- src/components/experiencia/Experiencia.js
+++ src/components/experiencia/Experiencia.js
@@ -1,4 +1,5 @@
 import bannerExperiencia from "./bannerExperiencia.png";
+import sustentabilidade from "./sustentabilidade.png"
 import "./experiencia.css";
 import pessoa from "./pessoa.png";
 
@@ -14,13 +15,22 @@ const Experiencia = () => {
     return(
         <main className="main-experiencia">
             <img src={bannerExperiencia} alt="a esperiecia" className="img-experiencia"/>
-            <div className="intro-experiencia">
-                <img src={pessoa} alt="pessoa feliz" className="foto-experiencia"/>
-                <div className="texto-ex">
-                    <h6 className="info-experiencia">Acessibilidade e Inclusao</h6>
-                    <p className="p-experiencia">Nosso evento tenta abraçar um público mais amplo em todos os sentidos, de todas as idades, corpos, gostos e pensamentos!  Isso está presente no  espaço físico, na sinalização, no treinamento da equipe de apoio, na comunicação em libras, braile, visual e tátil. Além disso, dispomos de ingressos gratuitos para pessoas com deficiência e acompanhantes, pessoas idosas e crianças!</p>
+            <section className="intro-experiencia">
+                <div className="itens">
+                    <img src={pessoa} alt="pessoa feliz" className="foto-experiencia"/>
+                    <div className="texto-ex">
+                        <h6 className="info-experiencia">Acessibilidade e Inclusao</h6>
+                        <p className="p-experiencia">Nosso evento tenta abraçar um público mais amplo em todos os sentidos, de todas as idades, corpos, gostos e pensamentos!  Isso está presente no  espaço físico, na sinalização, no treinamento da equipe de apoio, na comunicação em libras, braile, visual e tátil. Além disso, dispomos de ingressos gratuitos para pessoas com deficiência e acompanhantes, pessoas idosas e crianças!</p>
+                    </div>
                 </div>
-            </div>
+                <div className="itens">
+                    <div className="texto-ex2">
+                        <h6 className="info-experiencia2">Sustentabilidade</h6>
+                        <p className="p-experiencia2">Queremos estar aqui celebrando daqui a 50, 100 anos! Por isso, levamos o futuro do nosso planeta a sério. Nosso festival é carbono zero, com utilização de copos retornáveis, coleta e destinação adequada de resíduos, e transporte coletivo.</p>
+                    </div>
+                    <img src={sustentabilidade} alt="pessoa feliz" className="foto-experiencia2"/>
+                </div>
+            </section>
         </main>
     );
 }
