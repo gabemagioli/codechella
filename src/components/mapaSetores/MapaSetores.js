9f662dc8319e20c72640b6fe1dd6459a38dd2880
Gabriel Magioli <gabrielmagioli1@gmail.com>
2023-05-30 20:51:55 -0300

    commit

--- src/components/mapaSetores/MapaSetores.js
+++ src/components/mapaSetores/MapaSetores.js
@@ -1,6 +1,11 @@
 import "./mapaSetores.css";
 import bannerSetores from "./bannerSetores.png";
 import mapaSetores from "./mapaSetores.png";
+import detalhePista from "./detalhePista.png";
+import detalhePistaPremium from "./detalhePistaPremium.png";
+import detalheCadeiras from "./detalheCadeiras.png";
+
+
 
 const MapaSetores = () => {
 
@@ -27,6 +32,23 @@ const MapaSetores = () => {
                 </div>
                 <div className="detalhes-setores">
                     <h5 className="titulo-detalhes">Mais detalhes sobre os setores:</h5>
+                    <div className="imagens-detalhes">
+                        <div className="conjunto">
+                            <img src={detalhePista} alt="pista" className="pista"/>
+                            <h6 className="detalhamento-titulo">Pista</h6>
+                            <p className="informacoes-detalhadas">Pista convencional, atendida pelos bares e banheiros das laterais do estádio. Espaço amplo, com local para sentar e descansar. Separada da pista premium por uma grade.</p>
+                        </div>
+                        <div className="conjunto">
+                            <img src={detalhePistaPremium} alt="pista premium" className="premium"/>
+                            <h6 className="detalhamento-titulo">Pista Premium</h6>
+                            <p className="informacoes-detalhadas">Pista mais próxima do palco, com acesso muito próximo a banheiros e bares num raio de 100m. Todas as pessoas idosas e/ou com deficiência têm direito a acesso gratuito a essa área.</p>
+                        </div>
+                        <div className="conjunto">
+                            <img src={detalheCadeiras} alt="cadeiras" className="cadeira"/>
+                            <h6 className="detalhamento-cadeira">Cadeiras</h6>
+                            <p className="informacoes-cadeira">Opção de cadeiras térreas ou elevadas. Acesso próximo a banheiros e bares, visão um pouco elevada em relação às pistas.</p>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
